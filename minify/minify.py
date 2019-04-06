807e314dfc6d8ca1ce1d2a91e60e71d3f13432f4
jrdnbradford <36420801+jrdnbradford@users.noreply.github.com>
2019-04-06 11:50:45 -0400

    Simplify creation of new filename.

--- minify/minify.py
+++ minify/minify.py
@@ -6,34 +6,32 @@ Creates new, minified .json or .xml files.
 
 
 class FileExtensionError(Exception):
-    """Custom exception inheriting from base class."""
+    """Raised when file extension is not .json or .xml."""
     pass
 
 
 def __create_new_filename(f, custom):
     """Returns new filename."""
-    f_path = f.split("/")
-    f_dirs = f_path[0] if len(f_path[:-1]) == 1 else "/".join(f_path[:-1])
-    f_full_name = f_path[len(f_path) - 1]
-    f_split = f_full_name.split(".")
-    f_short_name = ".".join(f_split[:-1])
-    f_ext = f_split[len(f_split) - 1]   
-    return f"{f_dirs}/{f_short_name}{custom}.{f_ext}"
+    f_list = f.split(".")
+    f_list[-1] = f"{custom}{f_list[-1]}"
+    return ".".join(f_list)
 
 
 def __get_file_extension(f):
     """Returns file extension."""
-    f_split = f.split(".")
-    f_ext = f_split[len(f_split) - 1]
-    return f_ext
+    return f.split(".")[-1] 
 
 
-def minify(f):
+def minify(f, custom="min."):
     """
     Creates a new .json or .xml file without newlines, tabs, 
     and (most?) unnecessary whitespace.
 
     f: String. Name of file containing valid JSON or XML. 
+
+    custom: Optional string. Used to create new file name. 
+    "min." is the default: filename.extension becomes 
+    filename.min.extension.
 
     If file extension is not .json or .xml, FileExtensionError
     is raised.
@@ -47,7 +45,7 @@ def minify(f):
         raise FileExtensionError(f"File extension must be .json or .xml, not .{f_ext}")
     
     with open(f, "r") as data:
-        new_f = __create_new_filename(f, ".min")
+        new_f = __create_new_filename(f, custom)
         with open(new_f, "w") as min_f:
             for line in data:
                 for char in chars:
